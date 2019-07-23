a3cb1ef8e29a9a26d559dbc5cf549d6b766c05c6
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-07-23 22:08:19 +0200

    feat: update link component to display images from 'react-icon'

--- theme/src/components/links.js
+++ theme/src/components/links.js
@@ -1,27 +1,31 @@
 import React from 'react'
-
+import { graphql, useStaticQuery } from 'gatsby'
+import * as IconFiSet from 'react-icons/fi'
 import styles from './links.module.css'
-import GitHub from "../../assets/icons/github.svg";
-import ShrinkMe from "../../assets/icons/shrink-me.svg";
-import LinkedIn from "../../assets/icons/linkedIn.svg";
-import Mail from "../../assets/icons/mail.svg";
 
-export default () => (
-  <div className={styles.links}>
-    <a href="https://github.com/Safi1012" rel="noopener noreferrer" target="_blank" aria-label="Link to my GitHub profile">
-      <GitHub alt="GitHub icon" />
-    </a>
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      minimalistConfig(id: {eq: "gatsby-theme-minimalist-config"}) {
+        socialList {
+          icon
+          iconSet
+          url
+          ariaLabel
+        }
+      }
+    }
+  `)
 
-    <a href="https://shrinkme.app" rel="noopener noreferrer" target="_blank" aria-label="Link to my side project named Shrink Me">
-      <ShrinkMe alt="Shrink Me icon" />
-    </a>
-
-    <a href="https://www.linkedin.com/in/filipe-santoscorrea" rel="noopener noreferrer" target="_blank" aria-label="Link to my LinkedIn profile">
-      <LinkedIn alt="LinkedIn icon" />
-    </a>
-    
-    <a href="mailto:mail@filipesantoscorrea.com" rel="noopener noreferrer" aria-label="Link to my email address">
-      <Mail alt="Mail icon" />
-    </a>
-  </div>
-)
+  return (
+    <div className={styles.links}>
+      {
+        data.minimalistConfig.socialList.map((social, index) => (
+          <a href={social.url} rel="noopener noreferrer" target="_blank" aria-label={social.ariaLabel} key={index}>
+            {React.createElement(IconFiSet[social.icon], {})}
+          </a>
+        ))
+      }
+    </div>
+  )
+}
