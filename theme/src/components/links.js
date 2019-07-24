c78a4722c8261aee67734a63b6b7a87bc3888523
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-07-24 21:36:23 +0200

    feat: implement dynamic icon selection

--- theme/src/components/links.js
+++ theme/src/components/links.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import * as IconFiSet from 'react-icons/fi'
 import styles from './links.module.css'
+import * as IconSets from '../utils/icons'
 
 export default () => {
   const data = useStaticQuery(graphql`
@@ -17,12 +17,30 @@ export default () => {
     }
   `)
 
+  const renderIcon = (iconName) => {
+    const fallbackIcon = IconSets['Fi']['FiAlertTriangle']
+    const iconSetId = iconName.substring(0, 2)
+
+    // check if IconSet exists
+    if (!/Fa|Io|Md|Ti|Go|Fi|Gi|Wi|Di/.test(iconSetId)) {
+      return React.createElement(fallbackIcon, {})
+    }
+
+    // check if iconName exists in selected iconSet
+    if (!iconName in IconSets[`${iconSetId}`]) {
+      return React.createElement(fallbackIcon, {})
+    }
+
+    const icons = IconSets[`${iconSetId}`]
+    return React.createElement(icons[iconName], {})
+  }
+
   return (
     <div className={styles.links}>
       {
         data.minimalistConfig.socialList.map((social, index) => (
           <a href={social.url} rel="noopener noreferrer" target="_blank" aria-label={social.ariaLabel} key={index}>
-            {React.createElement(IconFiSet[social.icon], {})}
+            {renderIcon(social.icon)}
           </a>
         ))
       }
---
+++ theme/src/utils/icons.js
@@ -0,0 +1,21 @@
+import * as Fa from 'react-icons/fa'
+import * as Io from 'react-icons/io'
+import * as Md from 'react-icons/md'
+import * as Ti from 'react-icons/ti'
+import * as Go from 'react-icons/go'
+import * as Fi from 'react-icons/fi'
+import * as Gi from 'react-icons/gi'
+import * as Wi from 'react-icons/wi'
+import * as Di from 'react-icons/di'
+
+export {
+  Fa,
+  Io,
+  Md,
+  Ti,
+  Go,
+  Fi,
+  Gi,
+  Wi,
+  Di
+}
