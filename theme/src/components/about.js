36d687c1b0046d3465d1c2b97aaabcb14c443d99
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-07-24 22:37:20 +0200

    feat: use data from theme api instead of siteMetadata

--- theme/src/components/about.js
+++ theme/src/components/about.js
@@ -5,19 +5,17 @@ import styles from './about.module.css'
 export default () => {
   const data = useStaticQuery(graphql`
     query {
-      site {
-        siteMetadata {
-          headline,
-          subheadline
-        }
+      minimalistConfig(id: {eq: "gatsby-theme-minimalist-config"}) {
+        headline
+        subHeadline
       }
     }
   `)
   
   return (
     <>
-      <h1 className={styles.headline}>{data.site.siteMetadata.headline}</h1>
-      <p className={styles.underline} dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.subheadline }}></p>
+      <h1 className={styles.headline}>{data.minimalistConfig.headline}</h1>
+      <p className={styles.underline} dangerouslySetInnerHTML={{ __html: data.minimalistConfig.subHeadline }}></p>
     </>
   )
 }
--- theme/src/pages/index.js
+++ theme/src/pages/index.js
@@ -12,13 +12,11 @@ import particleOptions from '../../assets/particles.json';
 export default () => {
   const data = useStaticQuery(graphql`
     query {
-      site {
-        siteMetadata {
-          title,
-          description,
-          siteUrl,
-          appName
-        }
+      minimalistConfig(id: {eq: "gatsby-theme-minimalist-config"}) {
+        title,
+        description,
+        siteUrl,
+        appName
       }
     }
   `)
@@ -30,14 +28,14 @@ export default () => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 
-        <title>{data.site.siteMetadata.title}</title>
-        <meta name="title" content={data.site.siteMetadata.title} />
-        <meta name="description" content={data.site.siteMetadata.description} />
-        <link rel="canonical" href={data.site.siteMetadata.siteUrl} />
+        <title>{data.minimalistConfig.title}</title>
+        <meta name="title" content={data.minimalistConfig.title} />
+        <meta name="description" content={data.minimalistConfig.description} />
+        <link rel="canonical" href={data.minimalistConfig.siteUrl} />
 
-        <meta name="application-name" content={data.site.siteMetadata.appName} />
+        <meta name="application-name" content={data.minimalistConfig.appName} />
         <meta name="theme-color" content="#48bfcd" />
-        <meta name="apple-mobile-web-app-title" content={data.site.siteMetadata.appName} />
+        <meta name="apple-mobile-web-app-title" content={data.minimalistConfig.appName} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="mobile-web-app-capable" content="yes" />
